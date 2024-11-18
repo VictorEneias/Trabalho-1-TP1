ecea4f18d736fcabfe9e412746d6978509ea8b37
João Pedro De Souza Lopes <joao.pedro.de.souza.lopes2016@gmail.com>
2024-11-18 15:23:11 -0300

    adicao de 2 verificadores

--- dominio.cpp
+++ dominio.cpp
@@ -2,24 +2,76 @@
 
 using namespace std;
 
-void AVALIACAO::verifica(int avaliacao){
-    if(avaliacao > 6 & avaliacao < 0){}
-    else{
+void AVALIACAO::verifica(int avaliacao)
+{
+    if (avaliacao > 6 & avaliacao < 0)
+    {
+    }
+    else
+    {
         throw invalid_argument("Argumento invalido");
     }
 }
 
-void CODIGO::verifica(string codigo){
-    if(codigo.length() < 7){}
-    else{
+void CODIGO::verifica(string codigo)
+{
+    if (codigo.length() < 7)
+    {
+    }
+    else
+    {
         throw invalid_argument("Argumento invalido");
     }
-    for(int i=0, i < codigo.lenght(), i++){
-
-
-
-
-
-
+    for (int i = 0; i < codigo.length(); i++)
+    {
+        if (isdigit(codigo[i]) || isalpha(codigo[i]))
+        {
+        }
+        else
+        {
+            // sugestao de adicionar codigo de erro exemplo
+            //  Argumento invalido cod:500 invalid char ou Argumento invalido: invalid char
+            throw invalid_argument("Argumento invalido");
+        }
     }
 }
+void DATA::verifica(string data){
+    if (data.length()==10)//00/00/00
+    {    
+        if (isdigit(data[0])&&isdigit(data[1])
+            &&isdigit(data[3])&&isdigit(data[4])
+            &&isdigit(data[6])&&isdigit(data[7]))
+        {
+            
+            int day=(int(data[0]*10))+(int(data[1]));
+            int month=(int(data[3]*10))+(int(data[4]));
+            int ano=(int(data[6]*10))+(int(data[7]));
+            if (ano<0)
+            {
+                throw invalid_argument("Argumento invalido");   
+            }
+            else if (month<1 || month>12)
+            {
+                throw invalid_argument("Argumento invalido");
+            }else if(day<1 || 
+/*dias de mes 30*/  (day>30 &&(month==)))
+/*dias de mes 31*/  (day>30 &&(month==)))
+/*dias de mes 30 e ano bissextos*/  (day>30 &&(month==)))
+/*dias de mes 31 e ano bissextos*/  (day>30 &&(month==)))
+
+            {
+                throw invalid_argument("Argumento invalido");
+            }
+            
+        }else
+        {
+            throw invalid_argument("Argumento invalido");
+        }
+        
+    }else
+    {
+        throw invalid_argument("Argumento invalido");
+    }
+}
+
+void DINHEIRO::verifica(float dinheiro)
--- dominio.h
+++ dominio.h
@@ -3,173 +3,184 @@
 
 using namespace std;
 
+class AVALIACAO
+{
+private:
+    int avaliacao;
+    void verifica(int);
 
-class AVALIACAO{
-    private:
-        int avaliacao;
-        void verifica(int);
+public:
+    int GetAvaliacao();
 
-    public:
-        int GetAvaliacao();
-
-        void SetAvaliacao(int);
-
+    void SetAvaliacao(int);
 };
 
-class CODIGO{
-    private:
-        string codigo;
-        void verifica(string);
+class CODIGO
+{
+private:
+    string codigo;
+    void verifica(string);
 
-    public:
-        string GetCodigo();
+public:
+    string GetCodigo();
 
-        void SetCodigo(string);
-
+    void SetCodigo(string);
 };
 
-class DATA{
-    private:
-        string data;
-        void verifica(string);
+class DATA
+{
+private:
+    string data;
+    void verifica(string);
 
-    public:
-        string GetData();
+public:
+    string GetData();
 
-        void SetData(string);
-
+    void SetData(string);
 };
 
-class DINHEIRO{
-    private:
-        float dinheiro;
-        void verifica(float);
-
-    public:
-        float GetDinheiro();
-
-        void SetDinheiro(float);
-
+class DINHEIRO
+{
+private:
+    float dinheiro;
+    void verifica(float);
+public:
+    float GetDinheiro();
+    void SetDinheiro(float);
 };
 
-class DURACAO{
-    private:
-        int duracao;
-        void verifica(int);
+class DURACAO
+{
+private:
+    int duracao;
+    void verifica(int);
 
-    public:
-        int GetDuracao();
+public:
+    int GetDuracao();
 
-        void SetDuracao(int);
-
+    void SetDuracao(int);
 };
 
-class HORARIO{
-    private:
-        string horario;
-        void verifica(string);
-
-    public:
-        string GetHorario();
-
-        void SetHorario(string);
-
+class HORARIO
+{
+private:
+    string horario;
+    void verifica(string);
+public:
+    string GetHorario();
+    void SetHorario(string);
 };
 
-class NOME{
-    private:
-        string nome;
-        void verifica(string);
+class NOME
+{
+private:
+    string nome;
+    void verifica(string);
 
-    public:
-        string GetNome();
+public:
+    string GetNome();
 
-        void SetNome(string);
-
+    void SetNome(string);
 };
 
-class SENHA{
-    private:
-        int senha;
-        void verifica(int);
+class SENHA
+{
+private:
+    int senha;
+    void verifica(int);
 
-    public:
-        int GetSenha();
+public:
+    int GetSenha();
 
-        void SetSenha(int);
-
+    void SetSenha(int);
 };
 
 //--------------------------------------------------------------------------------------------------------------
 
-inline void AVALIACAO::SetAvaliacao(int avaliacao){
+inline void AVALIACAO::SetAvaliacao(int avaliacao)
+{
     verifica(avaliacao);
-    this -> avaliacao = avaliacao;
+    this->avaliacao = avaliacao;
 }
 
-inline int AVALIACAO::GetAvaliacao(){
+inline int AVALIACAO::GetAvaliacao()
+{
     return avaliacao;
 }
 
-inline void CODIGO::SetCodigo(string codigo){
+inline void CODIGO::SetCodigo(string codigo)
+{
     verifica(codigo);
-    this -> codigo = codigo;
+    this->codigo = codigo;
 }
 
-inline string CODIGO::GetCodigo(){
+inline string CODIGO::GetCodigo()
+{
     return codigo;
 }
 
-inline void DATA::SetData(string data){
+inline void DATA::SetData(string data)
+{
     verifica(data);
-    this -> data = data;
+    this->data = data;
 }
 
-inline string DATA::GetData(){
+inline string DATA::GetData()
+{
     return data;
 }
 
-inline void DINHEIRO::SetDinheiro(float dinheiro){
+inline void DINHEIRO::SetDinheiro(float dinheiro)
+{
     verifica(dinheiro);
-    this -> dinheiro = dinheiro;
+    this->dinheiro = dinheiro;
 }
 
-inline float DINHEIRO::GetDinheiro(){
+inline float DINHEIRO::GetDinheiro()
+{
     return dinheiro;
 }
 
-inline void DURACAO::SetDuracao(int duracao){
+inline void DURACAO::SetDuracao(int duracao)
+{
     verifica(duracao);
-    this -> duracao = duracao;
+    this->duracao = duracao;
 }
 
-inline int DURACAO::GetDuracao(){
+inline int DURACAO::GetDuracao()
+{
     return duracao;
 }
 
-inline void HORARIO::SetHorario(string horario){
+inline void HORARIO::SetHorario(string horario)
+{
     verifica(horario);
-    this -> horario = horario;
+    this->horario = horario;
 }
 
-inline string HORARIO::GetHorario(){
+inline string HORARIO::GetHorario()
+{
     return horario;
 }
 
-inline void NOME::SetNome(string nome){
+inline void NOME::SetNome(string nome)
+{
     verifica(nome);
-    this -> nome = nome;
+    this->nome = nome;
 }
 
-inline string NOME::GetNome(){
+inline string NOME::GetNome()
+{
     return nome;
 }
 
-inline void SENHA::SetSenha(int senha){
+inline void SENHA::SetSenha(int senha)
+{
     verifica(senha);
-    this -> senha = senha;
+    this->senha = senha;
 }
 
-inline int SENHA::GetSenha(){
+inline int SENHA::GetSenha()
+{
     return senha;
 }
