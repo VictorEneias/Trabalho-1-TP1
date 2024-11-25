be60ed5c10c0e63bf8fe5216452534819cf29c27
Victor Eneias <victor.eneias@gmail.com>
2024-11-25 09:38:36 -0300

    adcionei a entidade no .h

--- entidade.h
+++ entidade.h
@@ -1,4 +1,23 @@
 #ifndef ENTIDADE_H_INCLUDED
 #define ENTIDADE_H_INCLUDED
 
+#include "dominio.h"
+
+using namespace std;
+
+class CONTA{
+    private:
+        CODIGO codigo;
+        SENHA senha;
+
+    public:
+
+        void SetCodigo(const CODIGO&);
+        void SetSenha(const SENHA&);
+        CODIGO GetCodigo() const; 
+        SENHA GetSenha() const;
+};
+
+
+
 #endif
