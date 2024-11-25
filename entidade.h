a03821f9dd50c81990c1e85705aa250697410e7b
Victor Eneias <victor.eneias@gmail.com>
2024-11-25 20:35:53 -0300

    mais entidades

--- entidade.h
+++ entidade.h
@@ -9,13 +9,51 @@ class CONTA{
     private:
         CODIGO codigo;
         SENHA senha;
-
     public:
-
         void SetCodigo(const CODIGO&);
         void SetSenha(const SENHA&);
         CODIGO GetCodigo() const; 
         SENHA GetSenha() const;
+};
+
+class VIAGEM{
+    private:
+        CODIGO codigo;
+        NOME nome;
+        AVALIACAO avaliacao;
+    public:
+        void SetCodigo(const CODIGO&);
+        void SetNome(const NOME&);
+        void SetAvaliacao(const AVALIACAO&);
+        CODIGO GetCodigo() const; 
+        NOME GetNome() const;
+        AVALIACAO GetAvaliacao() const;
+};
+
+class ATIVIDADE{
+    private:
+        CODIGO codigo;
+        NOME nome;
+        DATA data;
+        HORARIO horario;
+        DURACAO duracao;
+        DINHEIRO preco;
+        AVALIACAO avaliacao;
+    public:
+        void SetCodigo(const CODIGO&);
+        void SetNome(const NOME&);
+        void SetData(const DATA&);
+        void SetHorario(const HORARIO&);
+        void SetDuracao(const DURACAO&);
+        void SetPreco(const DINHEIRO&);
+        void SetAvaliacao(const AVALIACAO&);
+        CODIGO GetCodigo() const;
+        NOME GetNome() const;
+        DATA GetData() const;
+        HORARIO GetHorario() const;
+        DURACAO GetDuracao() const;
+        DINHEIRO GetPreco() const;
+        AVALIACAO GetAvaliacao() const;
 };
 
 
